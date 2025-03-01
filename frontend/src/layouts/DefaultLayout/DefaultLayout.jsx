dce53cc52065609f68ba4a16de489d9cb1ebf3d2
DTD <tiendat30404@gmail.com>
2025-03-01 09:38:03 +0700

    build structure folder reactjs

---
+++ frontend/src/layouts/DefaultLayout/DefaultLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/Header/Header'
+import MainContent from '../../pages/HomePage/MainContent/MainContent'
+
+export default function DefaultLayout() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+    </div>
+  )
+}

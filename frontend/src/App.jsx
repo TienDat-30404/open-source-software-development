fd30a9da95cf4e61ba5f9669eb63d49146d3179a
lahuuminh <lahuuminh678@gmail.com>
2025-03-22 14:44:22 +0700

    delete change app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,45 +1,41 @@
-import { Fragment, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Fragment, useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { publicRoutes } from './routes/publicRoutes';
 import ChatApp from './pages/Chat/Chat';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-     <ChatApp />
-    // <div>
-    //   <Router>
-    //     <Routes>
-    //       {publicRoutes.map((route, i) => {
-    //         let Page = route.page
-    //         let Layout = route.layout 
-    //         if(route.layout)
-    //         {
-    //           Layout = route.layout
-    //         }
-    //         else if(route.layout === null)
-    //         {
-    //           Layout = Fragment
-    //         }
-    //         return (
-    //           <Route
-    //             key = {i}
-    //             path = {route.path}
-    //             element = {
-    //               <Layout>
-    //                 <Page />
-    //               </Layout>
-    //             }
-    //           />
-    //         )
-    //       })}
-    //     </Routes>
-    //   </Router>
-    // </div>
-  )
+    <div>
+      <Router>
+        <Routes>
+          {publicRoutes.map((route, i) => {
+            let Page = route.page;
+            let Layout = route.layout;
+            if (route.layout) {
+              Layout = route.layout;
+            } else if (route.layout === null) {
+              Layout = Fragment;
+            }
+            return (
+              <Route
+                key={i}
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
-export default App
+export default App;

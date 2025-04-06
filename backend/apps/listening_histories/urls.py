91fb62428a5cf138a061cb10d465e8c6b3282ee8
lahuuminh <lahuuminh678@gmail.com>
2025-04-06 18:18:23 +0700

    add listeininghistory api

---
+++ backend/apps/listening_histories/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import ListeningHistoryAPIView
+
+urlpatterns = [
+    path('listening-history', ListeningHistoryAPIView.as_view(), name='listening-history'),
+]

7fedccdee46edacd5b9a919fa955d06fc1ab4482
lahuuminh <lahuuminh678@gmail.com>
2025-04-06 18:18:04 +0700

    update favorite and follow

--- backend/apps/favorites/urls.py
+++ backend/apps/favorites/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import LikeSongAPIView
 
 urlpatterns = [
-    path('<int:song_id>', LikeSongAPIView.as_view(), name='like-song'),
+    path('', LikeSongAPIView.as_view(), name='like-song'),
 ]
--- backend/apps/favorites/views.py
+++ backend/apps/favorites/views.py
@@ -10,21 +10,30 @@ from apps.songs.models import Song
 from django.shortcuts import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
 from ..utils.response import success_response,error_response
+from .serializers import FavoriteSerial
 class LikeSongAPIView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def post(self, request, song_id):
+    # permission_classes = [IsAuthenticated]
+    def get(self, request):
+        user=User.objects.first()
+        favorite = Favorite.objects.filter(user=user)
+        serializer = FavoriteSerial(favorite, many=True)
+        return success_response(data=serializer.data)
+    def post(self, request):
         """API để like một bài hát"""
+        user=User.objects.first()
+        song_id = request.data.get('song_id')
         song = get_object_or_404(Song, id=song_id)
-        favorite, created = Favorite.objects.get_or_create(user=request.user, song=song)
+        favorite, created = Favorite.objects.get_or_create(user=user, song=song)
 
         if created:
             return success_response(message= "Like bài hát thành công",code=status.HTTP_201_CREATED)
         return error_response(message="Bạn đã like bài hát này rồi")
-    def delete(self, request, song_id):
+    def delete(self, request):
         """API để unlike một bài hát"""
+        user=User.objects.first()
+        song_id = request.data.get('song_id')
         song = get_object_or_404(Song, id=song_id)
-        favorite = Favorite.objects.filter(user=request.user, song=song)
+        favorite = Favorite.objects.filter(user=user, song=song)
 
         if favorite.exists():
             favorite.delete()

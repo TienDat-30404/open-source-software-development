582ccc5e6053339ce0a0166607e3a571310e018b
DTD <tiendat30404@gmail.com>
2025-05-08 13:11:08 +0700

    udpate deploy frotend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -37,7 +37,7 @@ export default defineConfig(({ mode }) => {
     preview: {
       host: '0.0.0.0',
       port: 5173,
-      allowedHosts: ['*']
+      allowedHosts: ['*', env.VITE_HOST_FRONTEND]
     }
   }
 })
